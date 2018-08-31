9b23b2ce445aee0c7af0b7577930b14ee6d3dfbd
Luca Monfredo <luca@monfredo.it>
2018-08-31 13:41:57 +0200

    Rollup config, library skeleton

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+class JohnnyMnemonic extends React.Component {
+  public render() {
+    return (
+      <h1>Hello Johnny!</h1>
+    )
+  }
+}
+
+export default JohnnyMnemonic
